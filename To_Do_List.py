78cf2945f72ce5eb4202a210ac74caf78acfdbb7
Umar <attiqumarattiq@gmail.com>
2026-08-06 23:52:06 +0500

    Read tasks from json file present on hard drive

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,4 +1,18 @@
 import json
 import os
 
-DATABASE_FILE = "my_tasks.json"
+DATABASE_FILE = "my_tasks.json"         # Creates a json file where data will be stored
+
+#======================================================
+# STEP 1: SAVING & LOADING
+#======================================================
+
+def load_tasks_from_list():
+    if os.path.exists(DATABASE_FILE):
+        try:
+            with open(DATABASE_FILE, "r") as file:
+                return json.load(file)
+        except json.JSONDecoder:
+            return []
+    return []
+
