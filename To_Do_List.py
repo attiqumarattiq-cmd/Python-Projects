2e516f8997de342f96a040447f5c3ade9cf945d7
Umar <attiqumarattiq@gmail.com>
2026-08-07 23:54:06 +0500

    Saves python list to a text file on disk permanently

--- To_Do_List.py
+++ To_Do_List.py
@@ -8,6 +8,7 @@ DATABASE_FILE = "my_tasks.json"         # Creates a json file where data will be
 #======================================================
 
 def load_tasks_from_list():
+    '''This function reads tasks from JSON file from the hard drive'''
     if os.path.exists(DATABASE_FILE):
         try:
             with open(DATABASE_FILE, "r") as file:
@@ -15,4 +16,10 @@ def load_tasks_from_list():
         except json.JSONDecoder:
             return []
     return []
+
+def save_tasks_to_disk(tasks_list):
+    """Saves Python list to a text file on disk permanently."""
+    with open(DATABASE_FILE, "w") as file:
+        json.dump(tasks_list, file, indent=4)
+    
 
