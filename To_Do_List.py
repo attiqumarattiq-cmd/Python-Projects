3f8ca41ff6047e3a841a3a4567cf9ca17cce4a0d
Umar <attiqumarattiq@gmail.com>
2026-08-08 21:10:07 +0500

    Convert the data from python to json data formate

--- To_Do_List.py
+++ To_Do_List.py
@@ -7,20 +7,26 @@ DATABASE_FILE = "my_tasks.json"         # Creates a json file where data will be
 # STEP 1: SAVING & LOADING
 #======================================================
 
-def load_tasks_from_list():
-    '''This function reads tasks from JSON file from the hard drive'''
+def load_tasks_from_disk():
+    #'''Reads tasks from a json file on your hard drive'''
     if os.path.exists(DATABASE_FILE):
         try:
             with open(DATABASE_FILE, "r") as file:
                 return json.load(file)
-        except json.JSONDecoder:
-            return []
-    return []
-
-
-def save_tasks_to_disk(tasks_list):
-    """Saves Python list to a text file on disk permanently."""
-    with open(DATABASE_FILE, "w") as file:
-        json.dump(tasks_list, file, indent=4)
+        except json.JSONDecoderError:
+            return[]
+        return[]
     
+    
+def save_tasks_to_disk(tasks_list):
+    with open(DATABASE_FILE, "w") as file:
+        json.dump(tasks_list, file, index = 4)  
+        # converts python data to json data formate
+        #tasks_list: The first argument; the Python data structure you are saving.file: 
+        #The second argument:  The target file object where the data is being written
+        
+
+
+
+
 
