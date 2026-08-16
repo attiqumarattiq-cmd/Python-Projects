06d8ee45fcc694f70bf46bc97acf3ce3cfa5771a
Umar <attiqumarattiq@gmail.com>
2026-08-16 12:24:12 +0500

    Added the option section

--- Expense_Tracker_Project.py
+++ Expense_Tracker_Project.py
@@ -17,6 +17,24 @@ try:
 except(FileNotFoundError, json.JSONDecodeError):
     app_data ={"budget": 0.0, "expenses": []}
     
+    
+#===================================================================
+
+while True:
+    print("\n=========== Expense Tracker ==========\n")
+    print("1. Add new expense")
+    print("2. View data")
+    print("3. Budget Capacity")
+    print("4. Analyzing the ration on each section")
+    print("5. Exporting data to CSV file") 
+    print("6. Want to exist the Program")
+    print("\n======================================\n")
+    
+    choice = input("Choose option u want (1-6) :")
+    
+
+     
+    
 
         
     
